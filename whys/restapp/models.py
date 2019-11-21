1fa896ba825f74ed37713614698e479e87794097
Fasther <30392053+Fasther@users.noreply.github.com>
2019-11-21 17:14:43 +0100

    model

--- whys/restapp/models.py
+++ whys/restapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Data(models.Model):
+    id = models.DecimalField
+    created = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=180, blank=False)
+    data = models.TextField(max_length=2000, blank=True)
