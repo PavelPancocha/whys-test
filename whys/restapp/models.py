7235e5b193bdbd22357c7e1b2f3a196ff4fda217
Fasther <30392053+Fasther@users.noreply.github.com>
2019-11-22 13:20:00 +0100

    Serializers

--- whys/restapp/models.py
+++ whys/restapp/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Data(models.Model):
-    id = models.DecimalField
+    ed = models.DecimalField(decimal_places=0, max_digits=10, verbose_name="Id")
     created = models.DateTimeField(auto_now_add=True)
     name = models.CharField(max_length=180, blank=False)
     data = models.TextField(max_length=2000, blank=True)
