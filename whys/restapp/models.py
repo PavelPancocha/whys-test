20defc214857c0ab88607c38fac47786ab79daa5
Fasther <30392053+Fasther@users.noreply.github.com>
2019-11-28 18:00:32 +0100

    Unique together

--- whys/restapp/models.py
+++ whys/restapp/models.py
@@ -14,6 +14,7 @@ class Data(models.Model):
     name = models.CharField(max_length=180, blank=False)
     eid = models.DecimalField(decimal_places=0, max_digits=10, blank=False)
     data = models.TextField(max_length=1000, blank=True)
+    unique_together = ['name', 'eid']
 
     def __str__(self):
         return "{} - Id: {}".format(self.name, self.eid)
